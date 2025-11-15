f7367e7d3238ec76eba93a3acb327eb9d095f322
CurroAyala <f.ayaladiaz04@gmail.com>
2025-11-15 23:39:11 +0100

    Comando para cargar productos creado

--- productos/models.py
+++ productos/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Producto(models.Model):
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=100)
     descripcion = models.TextField(max_length=200, blank=True, null=True)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     imagen = models.ImageField(upload_to="productos/", null=True, blank=True)
+    enlace_img = models.URLField(max_length=200, blank=True, null=True)
     
     class Meta:
         verbose_name = "Producto"
