65437dac04d040a7cb9b6e928525047eb8e795a7
Javier Luque <javluqrui@alum.us.es>
2025-11-25 18:29:25 +0100

    feat: Mejora de la vista de admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html  # <--- IMPORTANTE: Importar esto
 from .models import CustomUser
 
 @admin.action(description='Marcar como Cliente VIP')
@@ -15,7 +16,7 @@ def quitar_vip(modeladmin, request, queryset):
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'nombre', 'telefono', 'is_staff', 'is_active', 'is_superuser', 'is_vip')
+    list_display = ('email', 'nombre', 'status_vip','telefono', 'is_staff', 'is_active', 'is_superuser', 'is_vip')
     list_editable= ('is_vip',)
     list_filter = ('is_staff', 'is_active', 'is_superuser', 'is_vip')
     search_fields = ('email', 'nombre', 'telefono')
@@ -36,5 +37,20 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('email', 'nombre', 'telefono', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser', 'is_vip'),
         }),
     )
+    
+    def status_vip(self, obj):
+        if obj.is_vip:
+            # Renderizamos una estrella dorada y texto en negrita
+            return format_html(
+                '<span style="color: #FFD700; font-weight: bold; font-size: 14px;">★ VIP</span>'
+            )
+        else:
+            # Devolvemos un string vacío o un guion para no ensuciar la vista
+            return "-"
+    
+    # Configuramos los metadatos de la columna
+    status_vip.short_description = 'Distinción'  # Título de la columna
+    status_vip.admin_order_field = 'is_vip'
 
     readonly_fields = ('fecha_ingreso', 'last_login')
+
